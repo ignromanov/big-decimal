3b34fdbe4e671df33970b9ac6f5b989a35d65657
Ignat <ign.romanov@gmail.com>
2024-11-13 16:45:30 +0500

    chore: update exports & global type

---
+++ src/core/index.ts
@@ -0,0 +1,5 @@
+export { BigDecimal, type BigDecimalTypes } from "./base";
+export * from "./implementations";
+
+// Then import all prototype extensions through the formatting index
+import "@/operations/formatting";
---
+++ src/global.d.ts
@@ -0,0 +1,5 @@
+declare global {
+  type BigDecimal = import("./core").BigDecimal;
+}
+
+export {};
